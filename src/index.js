5ab3ab164855624fec0be5d73bd324eb0a622112
khjkr <khj@zeonplex.co.kr>
2019-11-27 16:23:57 +0900

    electron base init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 500,
+    height: 500,
+    webPreferences: {
+      preload: join(__dirname, '/preload.js')
+    }
+  })
+
+  mainWindow.loadFile('./public/index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
